fdbe0e1a4a2397d6f8bc4dde596bebc2ea6d4d42
Akira Matsuda <ronnie@dio.jp>
2012-12-11 16:22:17 +0900

    record transitions

--- lib/roundabout.rb
+++ lib/roundabout.rb
@@ -1,5 +1,16 @@
-require "roundabout/version"
+require 'roundabout/version'
+require 'roundabout/recorder'
 
 module Roundabout
-  # Your code goes here...
+  def self.record_transition(from, to, method, type)
+    recorder.record_transition from, to, method, type
+  end
+
+  def self.recorder
+    @recorder ||= Recorder.new
+  end
+
+  def self.compile_page_transitions
+    recorder.transitions
+  end
 end

2d2481e492129627f729179d65b3790082350a65
Akira Matsuda <ronnie@dio.jp>
2012-12-11 16:20:56 +0900

    initial implementation of the recorder

---
+++ lib/roundabout/recorder.rb
@@ -0,0 +1,15 @@
+require 'set'
+
+module Roundabout
+  class Recorder
+    attr_reader :transitions
+
+    def initialize
+      @transitions = Set.new
+    end
+
+    def record_transition(from, to, method, type)
+      @transitions << {from: from, to: to, method: method, type: type}
+    end
+  end
+end

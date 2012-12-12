28614c8d77fdb2f54b591618c1b800938bfe4626
Akira Matsuda <ronnie@dio.jp>
2012-12-12 12:58:19 +0900

    Vizualize in png

--- app/controllers/roundabout/roundabout_controller.rb
+++ app/controllers/roundabout/roundabout_controller.rb
@@ -1,5 +1,6 @@
 require 'pp'
 require 'roundabout/application_controller'
+require 'graphviz'
 
 module Roundabout
   class RoundaboutController < ::Roundabout::ApplicationController
@@ -9,6 +10,21 @@ module Roundabout
       else
         []
       end
+      GraphViz.new(:G, type: :digraph, rankdir: 'LR') {|g|
+        @transitions.each do |t|
+          from = g.add_nodes t['from'], shape: 'box'
+          to = g.add_nodes t['to'], shape: 'box'
+          color = case t['type']
+          when 'redirect'
+            'red'
+          when 'form'
+            'purple'
+          else
+            'blue'
+          end
+          g.add_edges from, to, color: color
+        end
+      }.output(png: Rails.root.join('public/images/roundabout.png'))
     end
   end
 end

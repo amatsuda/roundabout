8a456ab03b5aa268f048b19faffc56148abf1261
Akira Matsuda <ronnie@dio.jp>
2012-12-14 19:59:09 +0900

    describe the usage when data was not found

--- app/controllers/roundabout/roundabout_controller.rb
+++ app/controllers/roundabout/roundabout_controller.rb
@@ -5,26 +5,26 @@ require 'graphviz'
 module Roundabout
   class RoundaboutController < ::Roundabout::ApplicationController
     def index
-      @transitions = if (json = Rails.root.join('doc/roundabout.json')).exist?
-        ActiveSupport::JSON.decode json.read
-      else
-        []
-      end
-      GraphViz.new(:G, type: :digraph, rankdir: 'LR') {|g|
-        @transitions.each do |t|
-          from = g.add_nodes t['from'], shape: 'box'
-          to = g.add_nodes t['to'], shape: 'box'
-          color = case t['type']
-          when 'redirect'
-            'red'
-          when 'form'
-            'purple'
-          else
-            'blue'
+      if (json = Rails.root.join('doc/roundabout.json')).exist?
+        transitions = ActiveSupport::JSON.decode json.read
+        GraphViz.new(:G, type: :digraph, rankdir: 'LR') {|g|
+          transitions.each do |t|
+            from = g.add_nodes t['from'], shape: 'box'
+            to = g.add_nodes t['to'], shape: 'box'
+            color = case t['type']
+            when 'redirect'
+              'red'
+            when 'form'
+              'purple'
+            else
+              'blue'
+            end
+            g.add_edges from, to, color: color
           end
-          g.add_edges from, to, color: color
-        end
-      }.output(png: Rails.root.join('public/images/roundabout.png'))
+        }.output(png: Rails.root.join('public/images/roundabout.png'))
+      else
+        render 'readme'
+      end
     end
   end
 end

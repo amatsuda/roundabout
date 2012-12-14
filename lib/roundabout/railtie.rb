0c52e5a60a1304333d2b2e7e38dfde7f0f4376b2
Akira Matsuda <ronnie@dio.jp>
2012-12-14 17:46:00 +0900

    record redirections

--- lib/roundabout/railtie.rb
+++ lib/roundabout/railtie.rb
@@ -4,6 +4,9 @@ require 'roundabout/engine'
 module Roundabout
   class Railtie < ::Rails::Railtie #:nodoc:
     initializer 'roundabout' do |app|
+      ActiveSupport.on_load(:action_controller) do
+        require 'roundabout/monkey/action_controller'
+      end
       ActiveSupport.on_load(:after_initialize) do
         if Rails.env.development?
           Rails.application.routes.append do

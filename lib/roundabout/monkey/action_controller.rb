b99261ce5f23b0d4274515ed8a68d77868b0cec1
Akira Matsuda <ronnie@dio.jp>
2012-12-14 19:41:43 +0900

    On Error Resume Next

--- lib/roundabout/monkey/action_controller.rb
+++ lib/roundabout/monkey/action_controller.rb
@@ -2,8 +2,12 @@ module ActionController
   module Redirecting
     def redirect_to_with_recording(options = {}, response_status = {})
       redirect_to_without_recording options, response_status
-      h = @_request.env['action_dispatch.request.path_parameters'].with_indifferent_access
-      Roundabout.record_transition "#{h[:controller]}##{h[:action]}", Roundabout.normalize_url(self.location), :get, :redirect
+      begin
+        h = @_request.env['action_dispatch.request.path_parameters'].with_indifferent_access
+        Roundabout.record_transition "#{h[:controller]}##{h[:action]}", Roundabout.normalize_url(self.location), :get, :redirect
+      rescue => e
+        p e
+      end
     end
     alias_method_chain :redirect_to, :recording
   end
--- lib/roundabout/monkey/capybara.rb
+++ lib/roundabout/monkey/capybara.rb
@@ -3,16 +3,24 @@ require 'capybara'
 class Capybara::RackTest::Browser
   def follow_with_recording(method, path, attributes = {})
     return if path.gsub(/^#{request_path}/, '').start_with?('#')
-    Roundabout.record_transition Roundabout.normalize_url(URI.parse(driver.current_url).path), Roundabout.normalize_url(path), method, :link
+    begin
+      Roundabout.record_transition Roundabout.normalize_url(URI.parse(driver.current_url).path), Roundabout.normalize_url(path), method, :link
+    rescue => e
+      p e
+    end
 
     follow_without_recording method, path, attributes
   end
   alias_method_chain :follow, :recording
 
   def submit_with_recording(method, path, attributes)
-    p = (path.nil? || path.empty?) ? request_path : path
-    m = attributes['_method'] || method
-    Roundabout.record_transition Roundabout.normalize_url(URI.parse(driver.current_url).path), Roundabout.normalize_url(p, m), m, :form
+    begin
+      p = (path.nil? || path.empty?) ? request_path : path
+      m = attributes['_method'] || method
+      Roundabout.record_transition Roundabout.normalize_url(URI.parse(driver.current_url).path), Roundabout.normalize_url(p, m), m, :form
+    rescue => e
+      p e
+    end
 
     submit_without_recording method, path, attributes
   end

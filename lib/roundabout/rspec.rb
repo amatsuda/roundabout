1b5e9f9621f983922e33bc3f6aee5d0f2862aeab
Akira Matsuda <ronnie@dio.jp>
2013-06-26 14:26:03 +0900

    oops!

--- lib/roundabout/rspec.rb
+++ lib/roundabout/rspec.rb
@@ -2,7 +2,7 @@ RSpec.configure do |config|
   config.after :suite do
     transitions = Roundabout.compile_page_transitions
     unless transitions.empty?
-      Dir.mkdir Rails.root.join('doc')
+      Dir.mkdir Rails.root.join('doc') unless Dir.exist?(Rails.root.join('doc'))
       Rails.root.join('doc/roundabout.json').open('w') {|f| f.write transitions.to_json}
     end
   end

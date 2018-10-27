9dfdd8bbd7690862d14da2573a15f9825aaaa3f2
Akira Matsuda <ronnie@dio.jp>
2018-10-27 18:26:47 +0900

    Maybe we don't need the RackTest support anymore

--- lib/roundabout/monkey/capybara.rb
+++ lib/roundabout/monkey/capybara.rb
@@ -35,35 +35,7 @@ module Roundabout
         end
       end
     end
-
-    module RackTest
-      module Browser
-        def follow(method, path, attributes = {})
-          return if path.gsub(/^#{request_path}/, '').start_with?('#')
-          begin
-            Roundabout.record_transition Roundabout.normalize_url(URI.parse(driver.current_url).path), Roundabout.normalize_url(path, method), method, :link
-          rescue => e
-            p e
-          end
-
-          super
-        end
-
-        def submit(method, path, attributes)
-          begin
-            p = (path.nil? || path.empty?) ? request_path : path
-            m = attributes['_method'] || method
-            Roundabout.record_transition Roundabout.normalize_url(URI.parse(driver.current_url).path), Roundabout.normalize_url(p, m), m, :form
-          rescue => e
-            p e
-          end
-
-          super
-        end
-      end
-    end
   end
 end
 
 ::Capybara::Node::Element.prepend Roundabout::Capybara::Node::Element
-::Capybara::RackTest::Browser.prepend Roundabout::Capybara::RackTest::Browser

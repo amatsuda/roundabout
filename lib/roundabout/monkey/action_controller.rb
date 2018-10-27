7f3944482a31e929b8a6c75912afc3bb946c369b
Akira Matsuda <ronnie@dio.jp>
2018-10-27 18:13:48 +0900

    AMC => prepend

--- lib/roundabout/monkey/action_controller.rb
+++ lib/roundabout/monkey/action_controller.rb
@@ -1,14 +1,18 @@
-module ActionController
-  module Redirecting
-    def redirect_to_with_recording(options = {}, response_status = {})
-      redirect_to_without_recording options, response_status
-      begin
-        h = @_request.env['action_dispatch.request.path_parameters'].with_indifferent_access
-        Roundabout.record_transition "#{h[:controller]}##{h[:action]}", Roundabout.normalize_url(self.location), :get, :redirect
-      rescue => e
-        p e
+module Roundabout
+  module ActionController
+    module Redirecting
+      def redirect(options = {}, response_status = {})
+        super
+
+        begin
+          h = @_request.env['action_dispatch.request.path_parameters'].with_indifferent_access
+          Roundabout.record_transition "#{h[:controller]}##{h[:action]}", Roundabout.normalize_url(self.location), :get, :redirect
+        rescue => e
+          p e
+        end
       end
     end
-    alias_method_chain :redirect_to, :recording
   end
 end
+
+::ActionController::Redirecting.prepend Roundabout::ActionController::Redirecting
--- lib/roundabout/monkey/capybara.rb
+++ lib/roundabout/monkey/capybara.rb
@@ -1,28 +1,34 @@
 require 'capybara'
 
-class Capybara::RackTest::Browser
-  def follow_with_recording(method, path, attributes = {})
-    return if path.gsub(/^#{request_path}/, '').start_with?('#')
-    begin
-      Roundabout.record_transition Roundabout.normalize_url(URI.parse(driver.current_url).path), Roundabout.normalize_url(path, method), method, :link
-    rescue => e
-      p e
+module Roundabout
+  module Capybara
+    module RackTest
+      module Browser
+        def follow(method, path, attributes = {})
+          return if path.gsub(/^#{request_path}/, '').start_with?('#')
+          begin
+            Roundabout.record_transition Roundabout.normalize_url(URI.parse(driver.current_url).path), Roundabout.normalize_url(path, method), method, :link
+          rescue => e
+            p e
+          end
+
+          super
+        end
+
+        def submit(method, path, attributes)
+          begin
+            p = (path.nil? || path.empty?) ? request_path : path
+            m = attributes['_method'] || method
+            Roundabout.record_transition Roundabout.normalize_url(URI.parse(driver.current_url).path), Roundabout.normalize_url(p, m), m, :form
+          rescue => e
+            p e
+          end
+
+          super
+        end
+      end
     end
-
-    follow_without_recording method, path, attributes
   end
-  alias_method_chain :follow, :recording
-
-  def submit_with_recording(method, path, attributes)
-    begin
-      p = (path.nil? || path.empty?) ? request_path : path
-      m = attributes['_method'] || method
-      Roundabout.record_transition Roundabout.normalize_url(URI.parse(driver.current_url).path), Roundabout.normalize_url(p, m), m, :form
-    rescue => e
-      p e
-    end
-
-    submit_without_recording method, path, attributes
-  end
-  alias_method_chain :submit, :recording
 end
+
+::Capybara::RackTest::Browser.prepend Roundabout::Capybara::RackTest::Browser

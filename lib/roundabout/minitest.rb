b451e425d339d616e40ea506c4ab2ee9bf909ac4
Akira Matsuda <ronnie@dio.jp>
2018-10-27 18:39:52 +0900

    The minitest hook

---
+++ lib/roundabout/minitest.rb
@@ -0,0 +1,4 @@
+Minitest.after_run do
+  transitions = Roundabout.compile_page_transitions
+  Rails.root.join('tmp/roundabout.json').open('w') {|f| f.write transitions.to_json} unless transitions.empty?
+end

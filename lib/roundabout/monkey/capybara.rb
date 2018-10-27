d975354f97e6ba7dfe8f27d6d7a8533666a84e42
Akira Matsuda <ronnie@dio.jp>
2018-10-27 18:16:10 +0900

    Now we're able to record all kinds of drivers

--- lib/roundabout/monkey/capybara.rb
+++ lib/roundabout/monkey/capybara.rb
@@ -2,6 +2,40 @@ require 'capybara'
 
 module Roundabout
   module Capybara
+    module Node
+      module Element
+        def click(*keys, **offset)
+          path_from = session.current_path
+          case tag_name
+          when 'input'
+            type = :form
+            form = ancestor 'form'
+            path_to = form[:action]
+            method = form[:method]
+            begin
+              method = form.first('input[type=hidden][name=_method]', visible: false).value
+            rescue ::Capybara::ExpectationNotMet
+            end
+          when 'a'
+            type = :link
+            path_to = self[:href]
+            method = self[:'data-method'] || :get
+          end
+
+          ret = super
+
+          if path_from != path_to
+            begin
+              Roundabout.record_transition Roundabout.normalize_url(path_from), Roundabout.normalize_url(path_to, method), method, type
+            rescue => e
+              p e
+            end
+          end
+          ret
+        end
+      end
+    end
+
     module RackTest
       module Browser
         def follow(method, path, attributes = {})
@@ -31,4 +65,5 @@ module Roundabout
   end
 end
 
+::Capybara::Node::Element.prepend Roundabout::Capybara::Node::Element
 ::Capybara::RackTest::Browser.prepend Roundabout::Capybara::RackTest::Browser

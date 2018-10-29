26c6d28d282a94c025945096d5bc73035f01c594
Akira Matsuda <ronnie@dio.jp>
2018-10-29 15:06:46 +0900

    Clicked button may not always have an ancestor form

--- lib/roundabout/monkey/capybara.rb
+++ lib/roundabout/monkey/capybara.rb
@@ -11,12 +11,15 @@ module Roundabout
           case tag_name
           when 'input'
             type = :form
-            form = ancestor 'form'
-            path_to = form[:action]
-            method = form[:method]
             begin
-              method = form.first('input[type=hidden][name=_method]', visible: false).value
-            rescue ::Capybara::ExpectationNotMet
+              form = ancestor 'form'
+              path_to = form[:action]
+              method = form[:method]
+              begin
+                method = form.first('input[type=hidden][name=_method]', visible: false).value
+              rescue ::Capybara::ExpectationNotMet
+              end
+            rescue ::Capybara::ElementNotFound
             end
           when 'a'
             if (method = self[:'data-method'])
@@ -30,7 +33,7 @@ module Roundabout
 
           ret = super
 
-          if path_from != path_to
+          if path_from && path_to && (path_from != path_to)
             begin
               Roundabout.record_transition Roundabout.normalize_url(path_from), Roundabout.normalize_url(path_to, method), method, type
             rescue => e

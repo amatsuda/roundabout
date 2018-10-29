e234b39b8f6c3e79d29f908e5cfcb183c1de0023
Akira Matsuda <ronnie@dio.jp>
2018-10-30 08:14:45 +0900

    Formatting

--- lib/roundabout/railtie.rb
+++ lib/roundabout/railtie.rb
@@ -8,25 +8,27 @@ module Roundabout
     initializer 'roundabout' do |app|
       if ENV['ROUNDABOUT']
         if Rails::VERSION::MAJOR >= 5
-          ActiveSupport.on_load(:action_dispatch_integration_test) do
+          ActiveSupport.on_load :action_dispatch_integration_test do
             require 'roundabout/monkey/capybara'
             require 'roundabout/rspec' if defined? RSpec
             require 'roundabout/minitest' if defined? Minitest
           end
         else
-          ActiveSupport.on_load(:active_support_test_case) do
+          ActiveSupport.on_load :active_support_test_case do
             require 'roundabout/monkey/capybara'
             require 'roundabout/rspec' if defined? RSpec
             require 'roundabout/minitest' if defined? Minitest
           end
         end
-        ActiveSupport.on_load(:action_controller) do
+
+        ActiveSupport.on_load :action_controller do
           require 'roundabout/monkey/action_controller'
         end
-        ActiveSupport.on_load(:after_initialize) do
+
+        ActiveSupport.on_load :after_initialize do
           if Rails.env.development?
             Rails.application.routes.append do
-              mount Roundabout::Engine, :at => '/roundabout'
+              mount Roundabout::Engine, at: '/roundabout'
             end
           end
         end

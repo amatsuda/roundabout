955b88915cdfe86590a7c676a0f5f2cb9b8fc098
Akira Matsuda <ronnie@dio.jp>
2018-10-29 10:20:40 +0900

    Now that the scripts are written in vanilla JS

--- app/assets/javascripts/roundabout/roundabout.js
+++
@@ -1,8 +0,0 @@
-function drawArrows() {
-  window.paper = Raphael(0, 0, document.getElementById('roundabout').getAttribute('data-svg-width'), document.getElementById('roundabout').getAttribute('data-svg-height'));
-
-  eval(window.raw_edges).forEach(function(e) {
-    var path = "M" + e[0].replace("e,", "") + "L" + e[1].replace("e,", "")
-    window.paper.path(path).attr({'stroke-width': 2, stroke: e[2], 'arrow-end': 'classic-wide-long'})
-  })
-}

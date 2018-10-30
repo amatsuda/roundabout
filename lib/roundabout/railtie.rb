8c65860cea794bbdf39eee5b633b172c65deda40
Akira Matsuda <ronnie@dio.jp>
2018-10-30 10:58:23 +0900

    No need for the envvar for viewing the development engine

--- lib/roundabout/railtie.rb
+++ lib/roundabout/railtie.rb
@@ -24,12 +24,12 @@ module Roundabout
         ActiveSupport.on_load :action_controller do
           require 'roundabout/monkey/action_controller'
         end
+      end
 
-        ActiveSupport.on_load :after_initialize do
-          if Rails.env.development?
-            Rails.application.routes.append do
-              mount Roundabout::Engine, at: '/roundabout'
-            end
+      ActiveSupport.on_load :after_initialize do
+        if Rails.env.development?
+          Rails.application.routes.append do
+            mount Roundabout::Engine, at: '/roundabout'
           end
         end
       end
